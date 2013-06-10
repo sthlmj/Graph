02f5430f77c32895ca4d7d0bda3d3f747603ca2a
SthlmJ <joehuld1@gmail.com>
2013-06-10 21:38:52 +0200

    cmitt

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,6 +1,9 @@
 /* 
  * Graph shortest path with Dijkstra
  * Labb 5 Uppgift 10
+ * For each node calculates the neighboring node value and mark neighboring nodes until target reached.
+ *
+ * Viktad riktad graf. Bestämma kortaste vägarna från ett hörn i grafen till alla andra hörn.
  *  
  */
 
@@ -10,90 +13,106 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 
-class Vertex implements Comparable<Vertex>
+/**
+ *Vertex nodes graph representation
+ */
+class Node implements Comparable<Node>
 {
     public final String name;
-    public Edge[] adjacencies;
+    public Path[] neighbor;
     public double minDistance = Double.POSITIVE_INFINITY;
-    public Vertex previous;
-    public Vertex(String argName) { name = argName; }
+    public Node previous;	//a reference to the previous vertex to get a shortest path from the source vertex to this vertex.
+    public Node(String argName) { name = argName; }
     public String toString() { return name; }
-    public int compareTo(Vertex other)
+    public int compareTo(Node other)	//vertex comparator
     {
         return Double.compare(minDistance, other.minDistance);
     }
 }
 
-class Edge
+/**
+ *Edges paths representing an edge that stores its weight and target vertex(the vertex it points to) directed
+ */
+class Path
 {
-    public final Vertex target;
-    public final double weight;
-    public Edge(Vertex argTarget, double argWeight)
-    { target = argTarget; weight = argWeight; }
+    public final Node target;
+    public final double distance;
+    public Path(Node argTarget, double argDistance)
+    { target = argTarget; distance = argDistance; }
 }
 
 public class Dijkstra
 {
-    public static void computePaths(Vertex source)
+	/**
+	 * Shortest path
+	 */
+    public static void computePaths(Node source)
     {
-        source.minDistance = 0.;
-        PriorityQueue<Vertex> vertexQueue = new PriorityQueue<Vertex>();
-      	vertexQueue.add(source);
+        source.minDistance = 0.;	//distance to startNode is 0
+        
+        /**
+         * visit each vertex u, always visiting vertex with smallest minDistance first
+         */
+        PriorityQueue<Node> nodQueue = new PriorityQueue<Node>();	
+      	nodQueue.add(source);
 
-	while (!vertexQueue.isEmpty()) {
-	    Vertex u = vertexQueue.poll();
+	while (!nodQueue.isEmpty()) {
+	    Node u = nodQueue.poll();	//retrieves and removes head of queue
 
-            // Visit each edge exiting u
-            for (Edge e : u.adjacencies)
+            // Visit each edge
+            for (Path d : u.neighbor)	//path = d : u
             {
-                Vertex v = e.target;
-                double weight = e.weight;
+                Node n = d.target;
+                double weight = d.distance;
                 double distanceThroughU = u.minDistance + weight;
-		if (distanceThroughU < v.minDistance) {
-		    vertexQueue.remove(v);
-		    v.minDistance = distanceThroughU ;
-		    v.previous = u;
-		    vertexQueue.add(v);
+		if (distanceThroughU < n.minDistance) {		//remove n from queueu
+		    nodQueue.remove(n);
+		    n.minDistance = distanceThroughU ;
+		    n.previous = u;
+		    nodQueue.add(n);	//re-add n to queueue
 		}
             }
         }
     }
 
-    public static List<Vertex> getShortestPathTo(Vertex target)
+    public static List<Node> getShortestPathTo(Node target)		//getting the shortest path
     {
-        List<Vertex> path = new ArrayList<Vertex>();
-        for (Vertex vertex = target; vertex != null; vertex = vertex.previous)
-            path.add(vertex);
+        List<Node> path = new ArrayList<Node>();
+        for (Node nod = target; nod != null; nod = nod.previous)
+            path.add(nod);
         Collections.reverse(path);
         return path;
     }
-
+    
+    /**
+     * main
+     */
     public static void main(String[] args)
     {
-    Vertex v0 = new Vertex("Stockholm");
-	Vertex v1 = new Vertex("Göteborg");
-	Vertex v2 = new Vertex("Malmö");
-	Vertex v3 = new Vertex("Uppsala");
-	Vertex v4 = new Vertex("Västerås");
+    Node n0 = new Node("Stockholm");
+	Node n1 = new Node("Göteborg");
+	Node n2 = new Node("Malmö");
+	Node n3 = new Node("Uppsala");
+	Node n4 = new Node("Västerås");
 
-	v0.adjacencies = new Edge[]{ new Edge(v1, 5),
-	                             new Edge(v2, 10),
-                               new Edge(v3, 8) };
-	v1.adjacencies = new Edge[]{ new Edge(v0, 5),
-	                             new Edge(v2, 3),
-	                             new Edge(v4, 7) };
-	v2.adjacencies = new Edge[]{ new Edge(v0, 10),
-                               new Edge(v1, 3) };
-	v3.adjacencies = new Edge[]{ new Edge(v0, 8),
-	                             new Edge(v4, 2) };
-	v4.adjacencies = new Edge[]{ new Edge(v1, 7),
-                               new Edge(v3, 2) };
-	Vertex[] vertices = { v0, v1, v2, v3, v4 };
-        computePaths(v0);	//compute path
-        for (Vertex v : vertices)
+	n0.neighbor = new Path[]{ new Path(n1, 5),
+	                             new Path(n2, 10),
+                               new Path(n3, 8) };
+	n1.neighbor = new Path[]{ new Path(n0, 5),
+	                             new Path(n2, 3),
+	                             new Path(n4, 7) };
+	n2.neighbor = new Path[]{ new Path(n0, 10),
+                               new Path(n1, 3) };
+	n3.neighbor = new Path[]{ new Path(n0, 8),
+	                             new Path(n4, 2) };
+	n4.neighbor = new Path[]{ new Path(n1, 7),
+                               new Path(n3, 2) };
+	Node[] nodes = { n0, n1, n2, n3, n4 };
+        computePaths(n0);	//compute path Source node here
+        for (Node n : nodes)
 	{
-	    System.out.println("Distance to " + v + ": " + v.minDistance);
-	    List<Vertex> path = getShortestPathTo(v);
+	    System.out.println("Distance to " + n + ": " + n.minDistance);
+	    List<Node> path = getShortestPathTo(n);
 	    System.out.println("Path: " + path);
 	}
     }
